da72faefc79bfbed8363f767511e12cab9373540
sota1235 <sota1235@gmail.com>
2019-10-05 15:44:12 +0900

    modify: use eslint instead of tslint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import logger from 'morgan';
-import {setRoutes} from "./routes";
+import { setRoutes } from './routes';
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
