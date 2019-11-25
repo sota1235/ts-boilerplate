e2a7180c9132ece3e1f38a92e044a02c280e0cad
sota1235 <sota1235@gmail.com>
2019-11-25 12:06:39 +0900

    fix: use environment value for port number

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import logger from 'morgan';
 import { setRoutes } from './routes';
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // TODO switch format for Prod
 app.use(logger('dev'));
